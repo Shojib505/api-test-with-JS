d38aa4c4f3410aa84aad678711ed47862f0187af
alish <alishojib12@gmail.com>
2022-04-19 14:53:40 +0600

    notes commit

---
+++ js/countries.js
@@ -0,0 +1,43 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+}
+loadCountries();
+
+const displayCountries = counties => {
+    // for (const country of counties) {
+    //     console.log(country);
+    // }
+    const countriesDiv = document.getElementById('countries');
+    counties.forEach(country => {
+        // console.log(country);
+        const div = document.createElement('div');
+        div.classList.add('country')
+        div.innerHTML = `
+            <h3>${country.name.official}</h3>
+            <p>${country.capital}</p>
+            <button onclick="loadCountryByName('${country.name.common}')">Details</button>
+        `;
+        countriesDiv.appendChild(div);
+    });
+}
+//there is a problem with api link 
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/rest/v2/name/${name}`;
+    console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]));
+
+}
+
+const displayCountryDetail = country => {
+    console.log(country);
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+        <h5>${country.name.common}</h4>
+        <p>population: ${country.population}</p>
+        <img width="200px" src="${country.flag}">
+    `
+}
